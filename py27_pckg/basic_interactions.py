69c97d94a90c02314130aac9cfac50beef9dca16
eliot-christon <eliot.christon@gmail.com>
2024-03-20 15:49:44 +0100

    a script for nao basic interactions

---
+++ py27_pckg/basic_interactions.py
@@ -0,0 +1,29 @@
+from naoqi import ALProxy
+from nao_ip import NAO_IP
+
+print("NAO_IP: ", NAO_IP)
+
+autonomousLife = ALProxy("ALAutonomousLife", NAO_IP, 9559)
+autonomousLife.setState("disabled")
+# solitary, interactive, safeguard, disabled
+
+posture = ALProxy("ALRobotPosture", NAO_IP, 9559)
+posture.goToPosture("Sit", 0.5)
+
+basic_awareness = ALProxy("ALBasicAwareness", NAO_IP, 9559)
+basic_awareness.setEnabled(True)
+basic_awareness.getTrackingMode()
+
+audio_device = ALProxy("ALAudioDevice", NAO_IP, 9559)
+audio_device.setOutputVolume(40)
+
+tts = ALProxy("ALTextToSpeech", NAO_IP, 9559)
+
+print(tts.getAvailableLanguages())
+print(tts.getAvailableVoices())
+tts.setVoice("naofrf")
+
+tts.say("Et pourquoi pas ?")
+
+leds = ALProxy("ALLeds", NAO_IP, 9559)
+leds.reset("AllLeds")
