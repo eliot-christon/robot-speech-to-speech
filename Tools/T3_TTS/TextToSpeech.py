64b1d774b4d4711ebababf6d581b4149beaa823b
eliot-christon <eliot.christon@gmail.com>
2024-03-20 17:54:15 +0100

    TTS is working

---
+++ Tools/T3_TTS/TextToSpeech.py
@@ -0,0 +1,77 @@
+import logging
+import torch
+from TTS.api import TTS
+import time
+
+class TextToSpeech:
+    """Class for real-time speech TTS from live wav file using a pre-trained audio model."""
+
+#%% CONSTRUCTOR ==========================================================================================================
+
+    def __init__(self, model_name:str, language:str, input_text_file:str, output_wav_file:str, speaker_wav_file:str):
+        self.__text_file = input_text_file
+        self.__wav_file = output_wav_file
+        self.__speaker_wav = speaker_wav_file
+        self.__language = language
+
+        self.__running = False
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.__tts = TTS(model_name).to(device)
+
+#%% METHODS ==============================================================================================================
+
+    def __read_text(self) -> str:
+        """Read the input text file and return the text as string"""
+        with open(self.__text_file, 'r') as file:
+            text = file.read().replace('\n', '')
+        return text
+
+#%% GETTERS AND SETTERS ==================================================================================================
+
+    def get_running(self) -> bool:
+        return self.__running
+    
+    def get_wav_file(self) -> str:
+        return self.__wav_file
+    
+    def set_wav_file(self, wav_file:str):
+        self.__wav_file = wav_file
+
+    def get_text_file(self) -> str:
+        return self.__text_file
+
+    def set_text_file(self, text_file:str):
+        self.__text_file = text_file
+
+    def get_speaker_wav(self) -> str:
+        return self.__speaker_wav
+    
+    def set_speaker_wav(self, speaker_wav:str):
+        self.__speaker_wav = speaker_wav
+    
+
+#%% START ================================================================================================================
+
+    def start(self):
+        """Start the TTS process"""
+
+        logging.info("SpeechToText: Starting the TTS...")
+
+        self.__running = True
+
+        text = self.__read_text()
+
+        if not text:
+            logging.error("SpeechToText: No text to synthesize.")
+            self.__running = False
+            return
+
+        # Generate the speech directly to a file
+        self.__tts.tts_to_file(
+            text        = text,
+            file_path   = self.__wav_file,
+            language    = self.__language,
+            speaker_wav = self.__speaker_wav
+        )
+
+        self.__running = False
