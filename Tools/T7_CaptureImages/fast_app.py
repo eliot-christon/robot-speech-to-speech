a039576e7be34dea55c50eca0ab3c048baf61cfd
eliot-christon <eliot.christon@gmail.com>
2024-03-22 09:55:33 +0100

    package imports utils tools

--- Tools/T7_CaptureImages/fast_app.py
+++ Tools/T7_CaptureImages/fast_app.py
@@ -1,61 +1,11 @@
-import logging
-import time
-import yaml
-import threading
-
-def load_yaml(file_path):
-    with open(file_path, 'r') as file:
-        try:
-            data = yaml.safe_load(file)
-            return data
-        except yaml.YAMLError as exc:
-            logging.error(exc)
-
-class ToolFastApp:
-
-    def __init__(self, command_dict, get_status_function, status_file, command_file):
-        self.__command_dict = command_dict
-        self.__get_status = get_status_function
-        self.__status_file = status_file
-        self.__command_file = command_file
-        self.__lock = threading.Lock()  # Add a lock for thread safety
-
-    def __erase_command(self):
-        """Erase the command from the command file (text file)"""
-        with open(self.__command_file, 'w') as file:
-            file.write("_")
-
-    def __read_command(self):
-        """Read the command from the command file (text file)"""
-        with open(self.__command_file, 'r') as file:
-            return file.read()
-
-    def __write_status(self):
-        """Write the status to the status file (text file)"""
-        with open(self.__status_file, 'w') as file:
-            file.write(str(self.__get_status()))
-
-    def __execute_command(self, command):
-        """Execute the command in a separate thread"""
-        logging.info("Executing command: " + command)
-        if command in self.__command_dict:
-            threading.Thread(target=self.__command_dict[command]).start()
-
-    def run(self):
-        while True:
-            with self.__lock:  # Acquire lock before reading and erasing command
-                command = self.__read_command()
-                if command != '_':  # Check if command is not empty
-                    self.__execute_command(command)
-                    self.__erase_command()
-            self.__write_status()
-            time.sleep(0.1)
-
-#%% ================================================================================================
+import sys
+import os
+pckg_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..'))
+sys.path.append(pckg_dir)
 
 import logging
 from CaptureImages import CaptureImages
-
+from Tools.utils import ToolFastApp, load_yaml
 
 if __name__ == '__main__':
     logging.basicConfig(format='[%(levelname)s] - %(asctime)s - %(message)s')
---
+++ Tools/__init__.py
@@ -0,0 +1,3 @@
+import sys
+import os
+pckg_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
