403cb4cbc4f94bfa74f47f30fe40e44dfb15e0f7
eliot-christon <eliot.christon@gmail.com>
2024-05-02 11:22:59 +0200

    T8 STT better stt with whisper and thresholds

--- Tools/T8_STT/SpeechToText.py
+++ Tools/T8_STT/SpeechToText.py
@@ -14,12 +14,36 @@ class SpeechToText:
         self.initialize_audio_model(model_size)
         self.__running = False
         self.__transcription = ""
+        self.__list_nothing_saids = [
+            " Thank you for watching.",
+            " Thanks for watching!",
+            " Thank you for watching.",
+            " Thank you.",
+            " Thank you. Bye.",
+            " ...",
+            " Merci d'avoir regardé cette vidéo !",
+            " Please subscribe to my channel.",
+            " If you enjoyed this video, please like, comment, subscribe, and share it with your friends.",
+            "ご視聴ありがとうございました",
+            "最後まで視聴してくださって 本当にありがとうございます",
+            " 다음 영상에서 만나요!",
+            " 수고하셨습니다.",
+            "好",
+            " MBC 뉴스 이재경입니다.",
+            " Ah!",
+        ]
 
 #%% METHODS ==============================================================================================================
 
     def initialize_audio_model(self, model:str):
         """Initialize the audio model with the given model size."""
         self.__audio_model = WhisperModel(model, device="cuda", compute_type="float16")
+    
+    def __nothing_said(self, text:str) -> bool:
+        """Check if the transcribed text is one of the predefined 'nothing said' phrases."""
+        if text in self.__list_nothing_saids:
+            return True
+        return False
 
     def __write_to_file(self):
         """Write the transcribed text to the output text file."""
@@ -45,6 +69,8 @@ class SpeechToText:
             try:
                 segments, info = self.__audio_model.transcribe(self.__wav_file)
                 text = "".join([segment.text for segment in segments])
+                if self.__nothing_said(text):
+                    text = ""
                 self.__transcription = text
                 self.__write_to_file()
                 time.sleep(0.1)
