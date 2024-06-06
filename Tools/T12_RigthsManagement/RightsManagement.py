2695a8c90a22b63639c53ee29c08db8f56444028
eliot-christon <eliot.christon@gmail.com>
2024-06-06 10:28:13 +0200

    first glance at T12 Rights Management

---
+++ Tools/T12_RigthsManagement/RightsManagement.py
@@ -0,0 +1,64 @@
+import logging
+import requests
+
+class RightsManagement:
+    """Class to control the RightsManagement through an ODM server"""
+
+#%% CONSTRUCTOR ==========================================================================================================
+
+    def __init__(self, input_url_api_request:str, input_person_reconized_file:str, input_people_folder:str, output_documents_autorized_file:str, output_actions_autorized_file:str):
+        self.__input_url_api_request           = input_url_api_request
+        self.__input_person_reconized_file     = input_person_reconized_file
+        self.__input_people_folder             = input_people_folder
+        self.__output_documents_autorized_file = output_documents_autorized_file
+        self.__output_actions_autorized_file   = output_actions_autorized_file
+        self.__running = False
+
+#%% METHODS ==============================================================================================================
+
+    def __read_person_reconized(self) -> str:
+        """Read the person reconized"""
+        with open(self.__input_person_reconized_file, "r") as file:
+            return file.read().strip()
+    
+    def __get_level_of_duty(self, person:str) -> str:
+        """Get the level of duty of a person reading the people folder / person folder / level_of_duty.txt"""
+        with open(f"{self.__input_people_folder}/{person}/level_of_duty.txt", "r") as file:
+            return file.read().strip()
+    
+    def __api_request(self, person:str, level_of_duty:str) -> requests.Response:
+        """Make an API request to the RightsManagement API"""
+        url = f"{self.__input_url_api_request}?person={person}&level_of_duty={level_of_duty}" # TODO: Check if the URL is correct
+        response = requests.get(url)
+        return response
+
+    def __write_autorized_documents(self, autorisations:dict):
+        """Write the autorized documents in the output file"""
+        with open(self.__output_documents_autorized_file, "w") as file:
+            for document in autorisations["documents"]:
+                file.write(f"{document}\n")
+
+    def __write_autorized_actions(self, autorisations:dict):
+        """Write the autorized actions in the output file"""
+        with open(self.__output_actions_autorized_file, "w") as file:
+            for action in autorisations["actions"]:
+                file.write(f"{action}\n")
+    
+#%% GETTERS AND SETTERS ==================================================================================================
+
+    def get_running(self):
+        return self.__running
+
+#%% COMMANDS =============================================================================================================
+    def start(self):
+        self.__running = True
+
+        person        = self.__read_person_reconized()
+        level_of_duty = self.__get_level_of_duty(person)
+        response      = self.__api_request(person, level_of_duty)
+        autorisations = response.json()["autorisations"]
+        self.__write_autorized_documents(autorisations)
+        self.__write_autorized_actions(autorisations)
+
+        self.__running = False
+        logging.info("T12_RightsManagement: Finished.")
---
+++ Tools/T12_RigthsManagement/fast_app.py
@@ -0,0 +1,36 @@
+import sys
+import os
+pckg_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..'))
+sys.path.append(pckg_dir)
+
+import logging
+from RightsManagement import RightsManagement
+from utils import ToolFastApp, load_yaml, log_config
+
+if __name__ == '__main__':
+
+    log_config()
+
+    logging.info("Starting the T12_RightsManagement process...")
+
+    # Load the configuration parameters from the config file
+    params = load_yaml("Tools/parameters.yaml")["T12_RightsManagement"]
+
+    # Initialize the Leds object
+    rights_management = RightsManagement(
+        input_url_api_request           = params["input_url_api_request"],
+        input_person_reconized_file     = params["input_person_reconized_file"],
+        input_people_folder             = params["input_people_folder"],
+        output_documents_autorized_file = params["output_documents_autorized_file"],
+        output_actions_autorized_file   = params["output_actions_autorized_file"]
+    )
+    logging.info("T12_RightsManagement object initialized successfully.")
+
+    T12_fast_app = ToolFastApp(
+        command_dict        = {"start": rights_management.start},
+        get_status_function = rights_management.get_running,
+        status_file         = "Tools/T12_RightsManagement/fast_com/status.txt",
+        command_file        = "Tools/T12_RightsManagement/fast_com/command.txt"
+    )
+
+    T12_fast_app.run()
